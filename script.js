34cdd1eff43664dcb8789543d8cf113c2e445370
tanishqdesai <145229073+tanishqdesai@users.noreply.github.com>
2025-10-31 18:37:10 -0400

    Update script.js My opinion on identity
    My opinion on identity

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("Hello from JavaScript!I am Tanishq");
+console.log("Hello from JavaScript!I am Tanishq, I am who I choose to be, indentity is linear, non-liner and multi/poly linear matter of opinion");
